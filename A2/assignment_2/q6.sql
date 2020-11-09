745b77f40463b6b1423689f3b6538c399644f6f1
RickyY1689 <richard.jr.yang@gmail.com>
2020-11-09 16:23:26 -0500

    working on q6

--- A2/assignment_2/q6.sql
+++ A2/assignment_2/q6.sql
@@ -19,7 +19,7 @@ DROP VIEW IF EXISTS intermediate_step CASCADE;
 -- Define views for your intermediate steps here:
 
 -- Gets all holdings with only one contributor 
-DROP VIEW IF EXISTS author_publications;
+DROP VIEW IF EXISTS author_publications CASCADE;
 CREATE VIEW author_publications AS 
 SELECT holding, max(contributor) author
 FROM HoldingContributor 
@@ -27,44 +27,44 @@ GROUP BY holding
 HAVING count(contributor) = 1;
 
 -- Gets all books with only one contributor 
-DROP VIEW IF EXISTS single_author_books;
+DROP VIEW IF EXISTS single_author_books CASCADE;
 CREATE VIEW single_author_books AS 
 SELECT holding, author
 FROM author_publications JOIN Holding ON holding=id 
 WHERE htype = 'books';
 
 -- Get a count of all books written by each contributor 
-DROP VIEW IF EXISTS author_works; 
+DROP VIEW IF EXISTS author_works CASCADE;
 CREATE VIEW author_works AS 
 SELECT author, count(holding) books_written
 FROM single_author_books 
 GROUP BY author;
 
 -- Gets all checkouts of books with single authors 
-DROP VIEW IF EXISTS book_checkouts; 
+DROP VIEW IF EXISTS book_checkouts CASCADE; 
 CREATE VIEW book_checkouts AS 
 SELECT patron, author, count(c.holding) books_checked_out
 FROM Checkout c JOIN single_author_books s ON c.holding = s.holding
 GROUP BY patron, author;
 
 -- Gets reviews written by patrons who have checked out the books written by authors 
-DROP VIEW IF EXISTS book_reviews; 
+DROP VIEW IF EXISTS book_reviews CASCADE; 
 CREATE VIEW book_reviews AS 
 SELECT patron, author, count(review) books_reviewed, avg(stars) avg_rating
 FROM Review r JOIN single_author_books s ON r.holding = s.holding
 GROUP BY patron, author;
 
 -- Only keeps patrons who have reviewed all of an authors books they have checked out and averaged a 4 star rating
-DROP VIEW IF EXISTS commited_fans;
+DROP VIEW IF EXISTS commited_fans CASCADE;
 CREATE VIEW commited_fans AS 
-SELECT patron, author, books_checked_out
+SELECT b1.patron, b1.author, b1.books_checked_out
 FROM book_checkouts b1 JOIN book_reviews b2 ON (b1.patron=b2.patron AND b1.author=b2.author)
 WHERE b1.books_checked_out = b2.book_reviewed AND avg_rating >= 4;
 
 -- Gets all fans who have read all or all but one of an authors works
-DROP VIEW IF EXISTS devoted_fans;
+DROP VIEW IF EXISTS devoted_fans CASCADE;
 CREATE VIEW devoted_fans AS 
-SELECT patron, author
+SELECT c.patron, c.author
 FROM commited_fans c JOIN author_works a ON c.author=a.author
 WHERE c.books_checked_out >= a.books_written - 1;
 
