185c9da98e81f3abbfc5813e483d0943b0d08b9c
RickyY1689 <richard.jr.yang@gmail.com>
2020-11-09 17:33:55 -0500

    working on u2

--- A2/assignment_2/u2.sql
+++ A2/assignment_2/u2.sql
@@ -8,7 +8,7 @@ SET SEARCH_PATH TO Library, public;
 DROP VIEW IF EXISTS intermediate_step CASCADE;
 
 -- Get all checkouts from the branches in the php ward
-DROP VIEW IF EXISTS php_branches_checkouts CASCADE;
+DROP VIEW IF EXISTS downsview_checkouts CASCADE;
 CREATE VIEW php_branches_checkouts AS
 SELECT library branch, id, patron, holding, DATE(checkout_time) checkout_time
 FROM Checkout 
@@ -18,20 +18,20 @@ WHERE library = 'Downsview';
 DROP VIEW IF EXISTS not_returned_checkouts CASCADE;
 CREATE VIEW not_returned_checkouts AS 
 SELECT branch, id, patron, holding, DATE(checkout_time) checkout_time
-FROM php_branches_checkouts
+FROM downsview_checkouts
 WHERE id != ANY (SELECT checkout FROM Return);
 
 -- Determines the duedates for all items yet to be returned from php ward branches
 DROP VIEW IF EXISTS duedate_data CASCADE;
 CREATE VIEW duedate_data AS
-SELECT branch, patron, title, checkout_time, (checkout_time + 7) duedate
+SELECT branch, patron, n.holding, checkout_time, (checkout_time + 7) duedate
 FROM not_returned_checkouts n JOIN Holding h ON n.holding = h.id
 WHERE htype = 'books';
 
 -- Determine overdue books 
 DROP VIEW IF EXISTS overdue_data CASCADE;
 CREATE VIEW overdue_data AS
-SELECT branch, title, patron, ((SELECT current_date)-duedate)::INTEGER overdue
+SELECT branch, holding, patron, ((SELECT current_date)-duedate)::INTEGER overdue
 FROM duedate_data JOIN Patron ON patron = card_number 
 WHERE duedate < (SELECT current_date);
 
@@ -46,10 +46,14 @@ HAVING count(id) <= 5;
 -- Qualifying patrons who have less than 5 books checked out and none are overdue by more than 7 days
 DROP VIEW IF EXISTS qualifying_patrons CASCADE;
 CREATE VIEW qualifying_patrons AS 
-SELECT o.patron
+SELECT o.patron, o.holding
 FROM overdue_data o JOIN small_checkouts s ON o.patron = s.patron
 GROUP BY o.patron
 HAVING max(overdue) <= 7;
 
 -- Define views for your intermediate steps here, and end with a
 -- INSERT, DELETE, or UPDATE statement.
+
+UPDATE Checkout 
+SET checkout_time = checkout_time + interval '168 hours'
+WHERE holding = ANY (SELECT holding FROM qualifying_patrons);
