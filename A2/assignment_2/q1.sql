b2eb8da6017b2779864e84236120b9dd73be4244
RickyY1689 <richard.jr.yang@gmail.com>
2020-11-12 17:54:47 -0500

    working on q1

--- A2/assignment_2/q1.sql
+++ A2/assignment_2/q1.sql
@@ -23,6 +23,12 @@ DROP VIEW IF EXISTS intermediate_step CASCADE;
 
 -- Define views for your intermediate steps here:
 
+-- Sets up the view to store results into later with each branch having the required years
+DROP VIEW IF EXISTS branch_years CASCADE;
+CREATE VIEW branch_years AS 
+SELECT code branchId, years.y branchYear
+FROM LibraryBranch CROSS JOIN (VALUES (2015), (2016), (2017), (2018), (2019)) years(y);
+
 -- Associates all events with their respective branch 
 DROP VIEW IF EXISTS branch_events CASCADE;
 CREATE VIEW branch_events AS 
@@ -32,21 +38,21 @@ FROM LibraryEvent l1 JOIN LibraryRoom l2 ON l1.room = l2.id;
 -- Stores the events at branches at some year 
 DROP VIEW IF EXISTS branch_events_data CASCADE;
 CREATE VIEW branch_events_data AS 
-SELECT DISTINCT branchId, EXTRACT(YEAR FROM edate) eventYear, e.event eventId
+SELECT DISTINCT branchId, EXTRACT(YEAR FROM edate) branchYear, e.event eventId
 FROM branch_events b JOIN EventSchedule e ON b.eventId = e.event 
 WHERE EXTRACT(YEAR FROM edate) >= 2015 AND EXTRACT(YEAR FROM edate) <= 2019;
 
 -- Stores the average number of sessions for all events at a branch during some year 
 DROP VIEW IF EXISTS branch_events_sessions CASCADE;
 CREATE VIEW branch_events_sessions AS 
-SELECT branchId, eventYear, avg(e.event) avgSessions
+SELECT branchId, branchYear, avg(e.event) avgSessions
 FROM branch_events_data b JOIN EventSchedule e ON b.eventId = e.event 
 GROUP BY branchId, eventYear;
 
 -- Stores the number of registrations at a branch during some year 
 DROP VIEW IF EXISTS branch_events_reg CASCADE;
 CREATE VIEW branch_events_reg AS 
-SELECT branchId, eventYear, count(patron) 
+SELECT branchId, branchYear, count(patron) 
 FROM branch_events_data b JOIN EventSignUp e ON b.eventId = e.event 
 GROUP BY branchId, eventYear;
 
@@ -58,19 +64,31 @@ FROM LibraryCatalogue
 GROUP BY library;
 
 -- get num of checkouts at all branches 
-DROP VIEW IF EXISTS branch_holdings CASCADE;
-CREATE VIEW branch_holdings AS 
-SELECT library branchID, EXTRACT(YEAR from DATE(checkout_time)) eventYear, count(*)
+DROP VIEW IF EXISTS branch_num_checkouts CASCADE;
+CREATE VIEW branch_num_checkouts AS 
+SELECT library branchID, EXTRACT(YEAR from DATE(checkout_time)) branchYear, count(*)
 FROM Checkout
 GROUP BY branchId, eventYear
 HAVING EXTRACT(YEAR from DATE(checkout_time)) >= 2015 AND EXTRACT(YEAR from DATE(checkout_time)) <= 2019;
 
--- get average number of days between checkout and return for all items 
-DROP VIEW IF EXISTS branch_return_time CASCADE;
-CREATE VIEW branch_return_time AS 
-SELECT library branchID, EXTRACT(YEAR from DATE(checkout_time)) eventYear, id checkoutId, DATE(checkout_time) checkout_time
+-- get the checkout information formated
+DROP VIEW IF EXISTS branch_checkout CASCADE;
+CREATE VIEW branch_checkout AS 
+SELECT library branchID, EXTRACT(YEAR from DATE(checkout_time)) branchYear, id checkoutId, DATE(checkout_time) checkout_time
 FROM Checkout
 WHERE EXTRACT(YEAR from DATE(checkout_time)) >= 2015 AND EXTRACT(YEAR from DATE(checkout_time)) <= 2019;
 
+-- get average number of days between checkout and return for all items 
+DROP VIEW IF EXISTS branch_return_time CASCADE;
+CREATE VIEW branch_return_time AS 
+SELECT branchID, eventYear, avg(return_time) avg_return_time
+FROM (SELECT branchID, branchYear, (DATE(return_time) - checkout_time)::INTEGER return_time
+    FROM branch_checkout b JOIN Return r ON b.checkoutId = r.checkout) rs
+GROUP BY branchID, eventYear;
+
+SELECT * 
+FROM branch_years NATURAL LEFT JOIN branch_events_sessions;
+
+
 -- Your query that answers the question goes below the "insert into" line:
-insert into q1
+-- insert into q1
