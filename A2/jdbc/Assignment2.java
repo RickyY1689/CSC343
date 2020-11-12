e0f9d957fce3781c73131fbb02df70534f98bc2f
RickyY1689 <richard.jr.yang@gmail.com>
2020-11-11 21:30:46 -0500

    working on java

--- A2/jdbc/Assignment2.java
+++ A2/jdbc/Assignment2.java
@@ -254,7 +254,7 @@ public class Assignment2 {
       System.out.println(searchResults);
       System.out.println(a2.register("9909621460757", 11));
 
-      a2.item_return(59);
+      a2.item_return(255);
 
       // You can call your methods here to test them. It will not affect our 
       // autotester.
